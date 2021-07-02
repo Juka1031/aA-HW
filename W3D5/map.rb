f8b3a1613e83c6fa5cffcc097c82a52f29ffb5da
julian <juka1031@gmail.com>
2021-07-01 22:50:25 -0400

    finish W3D5 homework

---
+++ W3D5/map.rb
@@ -0,0 +1,41 @@
+require "byebug"
+class Map
+    def initialize
+        @map = Array.new() {Array.new()}
+    end
+
+    def set(key,value)
+        key_arr = []
+        @map.each do |sub_arr|
+            key_arr << sub_arr[0]
+        end
+
+        if key_arr.include?(key)
+            @map[key_arr.index(key)][1] = value
+        else
+            @map << [key,value]
+        end
+    end
+
+    def get(key)
+        key_arr = []
+        @map.each do |sub_arr|
+            key_arr << sub_arr[0]
+        end
+        @map[key_arr.index(key)][1]
+    end
+
+    def delete(key)
+        key_arr = []
+        @map.each do |sub_arr|
+            key_arr << sub_arr[0]
+        end
+        @map.delete_at(key_arr.index(key))if key_arr.include?(key)
+    end
+
+    def show
+        @map
+    end
+
+
+end
